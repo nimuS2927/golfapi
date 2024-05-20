c471820bbc12a57958d8577d66b519ea26a9468b
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-21 02:11:36 +1000

    created auth app

---
+++ auth/middlwares.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request, HTTPException, status, Depends
+from fastapi.security import HTTPBearer
+from core.config import c_project
+
+http_bearer = HTTPBearer(auto_error=False)
+app_auth = FastAPI(dependencies=[Depends(http_bearer)])
+
+# Some middlewares ...
+# @app_auth.middleware("http")
+# async def middleware_api_v1(request: Request, call_next):
+#     access_token = request.headers.get('X-Auth-Access-Token')
+#     if access_token:
+#         response = await call_next(request)
+#         return response
+#     raise HTTPException(
+#         status_code=status.HTTP_404_NOT_FOUND,
+#         detail=f"Invalid headers"
+#     )
