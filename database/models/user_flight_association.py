30d4c7d5a2bc9725843515f57d36d360139bb832
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-21 01:11:44 +1000

    created alembic migration for table flights and user_flight_association

---
+++ database/models/user_flight_association.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Table, Column, ForeignKey, UniqueConstraint
+
+from .base import Base
+
+
+user_flight_association_table = Table(
+    "user_flight_association",
+    Base.metadata,
+    Column("user_id", ForeignKey('users.id'), nullable=False),
+    Column("flight_id", ForeignKey('flights.id'), nullable=False),
+    UniqueConstraint("user_id", "flight_id", name="idx_unique_user_flight")
+)
+
