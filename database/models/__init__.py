50700d713ec167febb81c40881ebf2b2dbf4d3ab
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-21 01:12:17 +1000

    created alembic migration for table flights and user_flight_association

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -2,10 +2,12 @@ __all__ = (
     "Base",
     "User",
     "Admin",
+    "Flight",
 )
 
 from .base import Base
 
 from .admins import Admin
 from .user import User
+from .flights import Flight
 
--- database/models/user.py
+++ database/models/user.py
@@ -1,9 +1,10 @@
-from typing import Optional
+from typing import Optional, List
 
 from sqlalchemy import String, Float
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database.models import Base
+from database.models.user_flight_association import user_flight_association_table
 
 
 class User(Base):
@@ -11,3 +12,7 @@ class User(Base):
     first_name: Mapped[str] = mapped_column(String(50))
     last_name: Mapped[Optional[str]] = mapped_column(String(50))
     handicap: Mapped[float]
+    flights: Mapped[List['User']] = relationship(
+        back_populates='users',
+        secondary=user_flight_association_table,
+    )
