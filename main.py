abe32c600ab30bec7eac9192eaebfc1385bb43a8
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-21 18:47:23 +1000

    update main

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-# from api_v1 import app_db
-# from auth import app_auth
+from api_v1 import app_db
+from auth import app_auth
 from core import c_project
 
 
@@ -15,8 +15,8 @@ async def lifespan(app_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-# app.mount(c_project.api_v1_prefix, app_db)
-# app.mount(c_project.auth_v1_prefix, app_auth)
+app.mount(c_project.api_v1_prefix, app_db)
+app.mount(c_project.auth_v1_prefix, app_auth)
 
 
 if __name__ == '__main__':
