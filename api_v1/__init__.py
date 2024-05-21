f0b307a9c9e5d019338c972038372a9d51418c64
Sumin Ivan <sumin.ivan01@gmail.com>
2024-05-21 18:49:43 +1000

    include routers for api

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,5 +1,11 @@
 from .users import router as users_router
+from .tournaments import router as tournaments_router
+from .holes import router as holes_router
+from .flights import router as flights_router
 from .middlwares import app_db
 
 
 app_db.include_router(users_router)
+app_db.include_router(tournaments_router)
+app_db.include_router(holes_router)
+app_db.include_router(flights_router)
